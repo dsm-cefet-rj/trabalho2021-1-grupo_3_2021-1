23d1ac9c61679a19110326f9ad6065c858009adb
radhanama <radhanama@hotmail.com>
2021-08-11 08:37:02 -0300

    npx create

--- React_geprog/src/components/item.js
+++ React_geprog/src/components/item.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import img from '.../img/bicicleta,jpg';
 
-export default function item(){
+export default function item(name, desc, imag){
     return(
         <>
         <a className="reset-link" href="">
                 <div className="row resultado-busca">
                     <div className="col-4">
-                        <img className="img-fluid" src={img} alt="">
+                        <img className="img-fluid" src={imag} alt="">
                     </div>
                     <div className="col text-center">
-                        <h5>Bicicleta</h5>
-                        <p>Descrição do item</p>
+                        <h5>name</h5>
+                        <p>desc</p>
                     </div>
                 </div>
             </a>
