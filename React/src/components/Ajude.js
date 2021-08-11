19685e5d5d3e3e994918ff581d7268a19baa062e
radhanama <radhanama@hotmail.com>
2021-08-11 15:14:26 -0300

    react

---
+++ React/src/components/Ajude.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+
+export default function Ajude() {
+    return (
+        <section class="text-center">
+            <h3 class="mt-4 mb-3">Ajude seus vizinhos!</h3>
+
+            <div class="d-flex flex-wrap justify-content-evenly mb-3">
+                <div class="pedido-index">
+                    Resultado0
+                </div>
+                <div class="pedido-index">
+                    Resultado1
+                </div>
+                <div class="pedido-index">
+                    Resultado2
+                </div>
+                <div class="pedido-index">
+                    Resultado3
+                </div>
+                <div class="pedido-index">
+                    Resultado4
+                </div>
+                <div class="pedido-index">
+                    Resultado5
+                </div>
+            </div>
+        </section>
+    );
+}
