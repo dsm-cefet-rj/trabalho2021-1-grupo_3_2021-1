a7d5aa8ff296c35ad327d927fe6d6d93ee773736
radhanama <radhanama@hotmail.com>
2021-08-11 13:29:48 -0300

    correção

--- React_geprog/src/App.js
+++ React_geprog/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Sev from './components/sev.js'
+import fot from './img/bicicleta.jpg';
 
 function App() {
   return (
     <>
-      <Sev name="bicicleta"/>
+      <Sev foto={fot} name="bicicleta" desc="descrição do item"/>
     </>
   );
 }
--- React_geprog/src/components/sev.js
+++ React_geprog/src/components/sev.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import foto from '../img/bicicleta.jpg';
+
 
 export default function Sev(props) {
     return (
@@ -9,7 +9,7 @@ export default function Sev(props) {
                 <div className="row resultado-busca">
                 </div>
                 <div className="col-4">
-                    <img className="img-fluid" src={foto} alt="25" />
+                    <img className="img-fluid" src={props.foto} alt="25" />
                 </div>
                 <div className="col text-center">
                     <h5>{props.name}</h5>
