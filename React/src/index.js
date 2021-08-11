9edaf9f1f7b832678d324f3507c5a70f75a634ce
radhanama <radhanama@hotmail.com>
2021-08-11 18:50:11 -0300

    att index

--- React/src/index.js
+++ React/src/index.js
@@ -2,23 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar';
-<<<<<<< HEAD
 import MainMenu from './components/MainMenu';
 import Ajude from './components/Ajude'
-=======
-import Resultado from './resultado';
->>>>>>> 3ca3d0da22978c6a28bcac0f1afd4a02926d208b
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <NavBar />
     <MainMenu />
     <Ajude />
-=======
-    <Resultado />
->>>>>>> 3ca3d0da22978c6a28bcac0f1afd4a02926d208b
   </React.StrictMode>,
   document.getElementById('root')
 );
