31e797a4bbd643a837fe38be1c0ea2c8dc4cae5b
radhanama <radhanama@hotmail.com>
2021-08-11 09:06:30 -0300

    c

--- React_geprog/src/components/item.js
+++ React_geprog/src/components/item.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import img from '.../img/bicicleta,jpg';
 
-export default function item(name, desc, imag){
+export default function item(name, desc){
     return(
         <>
         <a className="reset-link" href="">
                 <div className="row resultado-busca">
-                    <div className="col-4">
-                        <img className="img-fluid" src={imag} alt="">
-                    </div>
+               <!--</div><div className="col-4">
+                        <img className="img-fluid" src={img} alt="">
+                    </div>-->
                     <div className="col text-center">
                         <h5>name</h5>
                         <p>desc</p>
