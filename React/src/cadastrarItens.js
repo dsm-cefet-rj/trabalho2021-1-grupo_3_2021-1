c0c9101e955dd15c9342e40ebe2c83da6cb956b6
radhanama <radhanama@hotmail.com>
2021-08-13 22:01:05 -0300

    comp 2

---
+++ React/src/cadastrarItens.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import NavBar from './components/NavBar.js'
+import ServCad from './components/ServiçosCadastro'
+import ItemCad from './components/ItensCadastro'
+
+
+export default function Procure() {
+    return (
+        <>
+            <NavBar />
+            <main class="container">
+        <form class="form-cadastro">
+            
+            <fieldset class="text-center">
+                <legend>
+                    <h3 class="mt-4">O que você deseja compartilhar?</h3>
+                </legend>
+                
+                <div>     
+                        <label class="form-check-label mx-1" for="flexRadio1">
+                            <input class="form-check-input" type="radio" name="radio-tipo" id="flexRadio1" value="coisas"/>
+                            Itens
+                        </label>
+                        
+                        <label class="form-check-label mx-1" for="flexRadio2">
+                            <input class="form-check-input" type="radio" name="radio-tipo" id="flexRadio2" value="servicos"/>
+                            Serviços
+                        </label> 
+                </div>
+            </fieldset>
+            
+            <fieldset class="responsive-form form-coisas">
+                <legend>
+                    Compartilhar itens
+                </legend>
+    
+               <ItemCad/>
+            </fieldset>
+
+            <fieldset class="responsive-form form-servicos">
+                <legend>
+                    Compartilhar serviços
+                </legend>
+
+                <ServCad/>
+            </fieldset>
+        </form>
+
+        
+    </main>
+        </>
+    );
+}
