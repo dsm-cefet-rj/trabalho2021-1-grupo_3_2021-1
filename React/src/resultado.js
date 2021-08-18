a8b46bf920d71d98abba5c80a0d172a711498085
Gabrel-Maga-23 <65359036+Gabrel-Maga-23@users.noreply.github.com>
2021-08-18 17:31:06 -0300

    Update resultado.js

--- React/src/resultado.js
+++ React/src/resultado.js
@@ -6,13 +6,16 @@ import NavBar from './components/NavBar.js'
 
 import Bike from './components/img/bicicleta.jpg'
 import chave from './components/img/chave-philips.jpg'
+import eletricista from './components/img/eletricista.JPG'
+import jardim from './components/img/jardim.jpg'
+import { Button } from 'bootstrap';
 
 
 const projetos = [
-    { foto: Bike, name: 'Bicicleta', desc: 'Bicicleta ruim' },
-    { foto: chave, name: 'chave', desc: 'philips' },
-    { foto: Bike, name: 'Bicicleta', desc: 'Bicicleta ruim' },
-    { foto: chave, name: 'chave', desc: 'philips' },
+    { foto: Bike, name: 'Bicicleta', desc: 'Bicicleta ruim'},
+    { foto: chave, name: 'chave', desc: 'philipis' },
+    { foto: eletricista, name: 'Eletricista', desc: 'eletricista'},
+    { foto: jardim, name: 'jardineiro', desc: 'bons serviços' },
 ]
 
 export default function Resultado() {
