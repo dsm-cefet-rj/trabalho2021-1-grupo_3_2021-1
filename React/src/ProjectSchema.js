c7d7fd785ae0ca73ccedcc7d2939edd0befceb9b
Gabrel-Maga-23 <65359036+Gabrel-Maga-23@users.noreply.github.com>
2021-09-01 17:10:12 -0300

    Add files via upload

---
+++ React/src/ProjectSchema.js
@@ -0,0 +1,18 @@
+import {string, object, number, setLocale} from 'yup';
+import { ptForm } from 'yup-locale-pt';
+
+
+setLocale(ptForm)
+
+const numMsg = "Números por favor."
+
+const strMsg = "Limite atingido"
+
+export let projetoSchema = object().shape(
+    {
+        id: number(),
+        name: string().required().max(30),
+        desc: string().required().max(100)
+
+    }
+)
