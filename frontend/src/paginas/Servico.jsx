1834f51acd8a2a9c7662964fed4be47c9037946c
Gabrel-Maga-23 <65359036+Gabrel-Maga-23@users.noreply.github.com>
2021-10-20 16:09:38 -0300

    Update Servico.jsx

--- frontend/src/paginas/Servico.jsx
+++ frontend/src/paginas/Servico.jsx
@@ -75,7 +75,7 @@ function LinhaServico(props) {
                 
 
                 <div className="col-4">
-                        <img className="img-fluid" src={foto} alt="" style={{borderRadius:"10px"}}/>
+                        <img className="img-fluid" src={props.servico.img} alt="" style={{borderRadius:"10px"}}/>
                     </div>
 
                     <div className="col text">
