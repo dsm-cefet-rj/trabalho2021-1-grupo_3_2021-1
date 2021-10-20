2cb0c5a9c2b61cd3d8e15b27b91b6511ecbc185a
Gabrel-Maga-23 <65359036+Gabrel-Maga-23@users.noreply.github.com>
2021-10-20 15:59:45 -0300

    Update listaPedido.jsx

--- frontend/src/paginas/listaPedido.jsx
+++ frontend/src/paginas/listaPedido.jsx
@@ -21,7 +21,7 @@ function LinhaPedido(props){
           
 
           <div className="col-4">
-                  <img className="img-fluid" src={foto} alt="" style={{borderRadius:"10px"}}/>
+                  <img className="img-fluid" src={props.pedido.img} alt="" style={{borderRadius:"10px"}}/>
               </div>
 
               <div className="col text">
