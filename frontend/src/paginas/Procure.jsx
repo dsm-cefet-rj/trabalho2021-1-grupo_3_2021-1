4b6c6a11a906880f3e0a1a7696965a29054ada5c
Gabrel-Maga-23 <65359036+Gabrel-Maga-23@users.noreply.github.com>
2021-10-20 15:55:22 -0300

    Update Procure.jsx

--- frontend/src/paginas/Procure.jsx
+++ frontend/src/paginas/Procure.jsx
@@ -7,20 +7,77 @@ import { Link } from "react-router-dom";
 
 function Procure () {
     return (
-        <div className="row resultado-busca">
+
+
+        <section className="container first-element">
+        <div  
+      
+      class="container-md"
+      style={{
+
+        backgroundColor:'dodgerblue',
+        borderRadius:'20px',
+        padding:'50px',
         
+
+
+      }}>
+
+<div  
+      
+      class="container-md"
+      style={{
+
+        backgroundColor:'blue',
+        borderRadius:'20px',
+        padding:'20px',
+        color:'white',
+
+
+      }}>
+        <h3 className="link-title" style={{fontSize:"45px"}}>Produto</h3>
         <Link to="/procureProd">
-        <br/><br/><br/>
-            <h3 className="link-title">Produto</h3>
+        <button type="button" className="btn btn-primary">Produtos em destaque</button>
         </Link>
-        <Link  to="/procurePed">
-            <h3 className="link-title">Pedido</h3>
-        </Link>
-        <Link to="/procureServ">
-            <h3 className="link-title">Serviço</h3>
-        </Link>
-        
         </div>
+        <br/><br/>
+        
+      <div
+      class="container-md"
+      style={{
+
+        backgroundColor:'blue',
+        borderRadius:'20px',
+        padding:'50px',
+        color:'white',
+
+
+      }}>
+        <h3 className="link-title" style={{fontSize:"45px"}}>Pedido</h3>
+        <Link  to="/procurePed">
+        <button type="button" className="btn btn-primary">Pedidos em destaque</button>
+        </Link>
+        </div>
+        <br/><br/>
+        <div  
+      
+      class="container-md"
+      style={{
+
+        backgroundColor:'blue',
+        borderRadius:'20px',
+        padding:'50px',
+        color:'white',
+
+
+      }}>
+        <h3 className="link-title" style={{fontSize:"45px"}}>Serviço</h3>
+        <Link to="/procureServ">
+        <button type="button" className="btn btn-primary">Serviços em destaque</button>
+        </Link>
+        </div>
+        </div>
+        </section>
     );
 }
 
